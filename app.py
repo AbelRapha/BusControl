9e82be6e976f9bcd418627478f4f896b6f94b337
Abel <79184789+AbelRapha@users.noreply.github.com>
2024-10-26 00:50:01 +0000

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import streamlit as st
+import pandas as pd
+
+df= pd.DataFrame()
+
+df[['horario_linha','numero_onibus']] = None
+
+def criar_lista_horarios(inicio,fimm,intervalos):
+    horarios=[]
+    hora_atual= inicio
+    for horario_limite in intervalos:
+        while hora_atual <= horario_limite:
+            horarios.append(hora_atual)
+            hora_atual += datetime.timedelta(minutes=intervalo)
+    return horarios 
 
 st.markdown('# Controle de Frota de Ônibus')
 
@@ -10,11 +24,12 @@ numero_passageiros_pico = st.sidebar.number_input('Digite o volume de passageiro
 
 numero_passageiros_normal = st.sidebar.number_input('Digite o volume de passageiros NORMAL')
 
-capacidade_dos_onibus_pico = st.sidebar.number_input('Digite a capacidade do ônibus PICO')
+capacidade_dos_onibus_pico = st.sidebar.number_input('Digite a capacidade do ônibus PICO',1)
 
-capacidade_dos_onibus_normal = st.sidebar.number_input('Digite a capacidade do ônibus NORMAL')
+capacidade_dos_onibus_normal = st.sidebar.number_input('Digite a capacidade do ônibus NORMAL',1)
 
 from datetime import time
+import datetime as dt
 
 horario_da_linha = st.sidebar.slider(
     "Selecione o intervalo do horário da linha:", value=(time(7, 00), time(8, 00))
@@ -29,29 +44,37 @@ horario_pico = st.sidebar.slider(
 
 # Coeficiente de Pico e Normal
 
-qp = numero_passageiros_pico/capacidade_dos_onibus_pico
+qp = (numero_passageiros_pico/capacidade_dos_onibus_pico)
 
 qn = numero_passageiros_normal/capacidade_dos_onibus_normal
 
 # Calculando Headway de Pico e Normal
 
-if qp and qn != 0:
+if qp !=0 and qn != 0:
     hp = 60/qp
     hn = 60/qn
 
 # Calculando Tempo de ciclo e Pico
 
-tc = extensao_ida_volta/velocidade_comercial
-tp = horario_pico[1] - horario_pico[0]
+    tc = extensao_ida_volta/velocidade_comercial
+    tp = horario_pico[1].hour - horario_pico[0].hour
 
 # Verificando a condição da cálculo da Frota de Pico e Normal
-if tc <= tp:
-    fp = tc/hp
-    fn = tc/hn
-else:
-    fp = (tp/hp) + (tc-tp)/hn
-    fn = tc/hn
+    if tc <= tp:
+      fp = tc/hp
+      fn = tc/hn
+    else:
+      fp = (tp/hp) + (tc-tp)/hn
+      fn = tc/hn
 
 # Desenvolvendo a tabela
+    if fp and fn:
+      intervalos = [(dt.time(horario_pico[0].hour,0),hp),(dt.time(horario_pico[0].hour,0),hn)]
+    
+      lista = criar_lista_horarios(horario_da_linha[0],horario_da_linha[1],intervalos)
 
-st.write(horario_pico)
+    #  st.write((dt.time(horario_pico[0].hour,0),hp))
+      hora_atual = (dt.time(horario_da_linha[0].hour,0)
+      for horario_limite, intervalo in intervalos:
+        while hora_atual.time  <= horario_limite:
+          st.write(hora_atual)
